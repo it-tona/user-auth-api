d5420acf677055415558e0f8f8c5183fb64b431c
Sodji Komlan Emelin Tona <aime@macaime.local>
2025-08-05 19:53:53 +0200

    deploy

---
+++ src/main/java/it/tona/user_auth_api/config/BaseServiceAndLogic.java
@@ -0,0 +1,14 @@
+package it.tona.user_auth_api.config;
+
+import org.slf4j.Logger;
+import org.springframework.stereotype.Component;
+
+import lombok.extern.slf4j.Slf4j;
+
+@Component
+@Slf4j
+public class BaseServiceAndLogic {
+    
+    public static Logger logger = log;
+
+}
